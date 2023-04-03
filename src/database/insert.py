3057a3c66b468f88c085ec4f39042738153a94d9
Bentley Hensel <bentley.hensel@civicactions.com>
2023-04-03 17:25:57 +0000

    Fixed commit issues & added debug logging

--- src/database/insert.py
+++ src/database/insert.py
@@ -1,9 +1,11 @@
 import psycopg2
 from database.access import connection
 from logger.config import logger
-from database.access import connection
 
 def execute_insert(query, params=None, fetchone=True):
+    logger.debug(f"🗄️✏️ Executing query: {query}")
+    logger.debug(f"🗄️✏️ Query parameters: {params}")
+
     """
     Execute a query on the database and return the result.
 
@@ -18,27 +20,38 @@ def execute_insert(query, params=None, fetchone=True):
     # Connect to the database
     conn = connection()
     conn.open()
+    logger.debug("🗄️✏️ Database connection opened")
 
     # Create a cursor
     cur = conn.conn.cursor()
 
-    # Execute the query
-    cur.execute(query, params)
+    try:
+        # Execute the query
+        cur.execute(query, params)
+        conn.conn.commit()
+        logger.info("🗄️✏️ Query executed and committed")
 
-    # Fetch the results if requested
-    result = None
-    if fetchone:
-        result = cur.fetchone() if cur.rowcount > 0 else None
-    else:
-        result = cur.fetchall()
+        # Fetch the results if requested
+        result = None
+        if fetchone:
+            result = cur.fetchone() if cur.rowcount > 0 else None
+        else:
+            result = cur.fetchall()
+            logger.debug(f'🗄️✏️ Fetched results: {result}')
+    except Exception as e:
+        logger.error(f"🗄️✏️ Error executing insert query: {e}")
+        result = None
 
     # Close the cursor and connection
     cur.close()
     conn.close()
+    logger.debug("🗄️✏️ Cursor and connection closed")
 
     return result
 
+
 def record_new_crawl(actor_id, started_by, crawl_uuid, crawl_type, domain_id, start_url):
+    logger.info(f"🗄️✏️ Recording new crawl: {crawl_uuid}")
     query = """
         INSERT INTO events.crawls (
             actor_id,
@@ -56,24 +69,17 @@ def record_new_crawl(actor_id, started_by, crawl_uuid, crawl_type, domain_id, st
         RETURNING crawl_uuid
         """
     result = execute_insert(query, (actor_id, started_by, crawl_uuid, crawl_type, domain_id, start_url))
-    logger.debug(f'Record New Crawl Output: {result} ')
+    logger.debug(f'🗄️✏️ Record New Crawl Output: {result} ')
     if result[0] == crawl_uuid:
-        logger.debug(f' Crawl {crawl_uuid} created... ')
+        logger.debug(f'🗄️✏️ Crawl {crawl_uuid} created... ')
         return True
     else:
-        logger.error(f'Record New Crawl had an issue with: {crawl_uuid} ')
+        logger.error(f'🗄️✏️ Record New Crawl had an issue with: {crawl_uuid} ')
         return False
 
 
-#    try:
- #       execute_insert(query, (actor_id, started_by, crawl_uuid, crawl_type, domain_id, start_url))
- #       return True
- #   except:
- #       return False
-
-
-
 def record_crawled_url(url, crawl_uuid, source_url):
+    logger.info(f"🗄️✏️ Recording crawled URL: {url}")
     query = """
         INSERT INTO results.urls (url,
             crawl_uuid,
@@ -88,9 +94,9 @@ def record_crawled_url(url, crawl_uuid, source_url):
     try:
         result = execute_insert(query, (url, crawl_uuid, crawl_uuid, source_url, source_url))
         action = result[0] if result else None
-        logger.info(f'URL Recorded: {url}')
+        logger.info(f'🗄️✏️ URL Recorded: {url}')
         return action
     except:
-        logger.error(f'Unable to record URL: {url}')
+        logger.error(f'🗄️✏️ Unable to record URL: {url}')
         return None
 
