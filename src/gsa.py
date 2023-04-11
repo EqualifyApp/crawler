c7c58243c5ddcf79ba7a6e2e1f150ac805d19f0d
Bentley Hensel <bentley.hensel@civicactions.com>
2023-04-11 14:40:32 +0000

    GSA Turbo

--- src/gsa.py
+++ src/gsa.py
@@ -1,23 +1,21 @@
-import requests
 import sys
+import requests
 from bs4 import BeautifulSoup
-# from multiprocessing import Pool
 from concurrent.futures import ThreadPoolExecutor
 from logger.config import logger
 from database.select import next_sitemap_url
 from database.insert import record_new_urls, create_new_crawl
-from database.update import update_sitemap_status
 
 
-def fetch_url(url):
-    sys.setrecursionlimit(10000)
+def fetch_sitemap_content(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         return response.text
     except requests.exceptions.RequestException as e:
-        logger.error(f"Error fetching URL: {url} - {str(e)}")
+        logger.error(f"Error fetching sitemap URL: {url} - {str(e)}")
         return None
+
 
 def parse_sitemap(sitemap_content):
     if not sitemap_content:
@@ -41,7 +39,7 @@ def crawl_sitemap(sitemap_url):
         current_sitemap = sitemaps_to_crawl.pop()
         if current_sitemap not in visited_sitemaps:
             visited_sitemaps.add(current_sitemap)
-            sitemap_content = fetch_url(current_sitemap)
+            sitemap_content = fetch_sitemap_content(current_sitemap)
             urls = parse_sitemap(sitemap_content)
 
             for url in urls:
@@ -53,47 +51,26 @@ def crawl_sitemap(sitemap_url):
     return found_urls
 
 
-
 def main(sitemap_url, sitemap_id, domain_id):
-    # Create new Crawl
     crawl_type = 'sitemap'
     agent = 'gsa_soup'
     crawl_id = create_new_crawl(crawl_type, sitemap_id, agent, domain_id)
-    # Get Started
-    visited_sitemaps = []
+
     raw_urls = crawl_sitemap(sitemap_url)
 
-   # with Pool() as p:
-    #    sitemap_contents = p.map(fetch_url, raw_urls)
-    with ThreadPoolExecutor() as executor:
-        sitemap_contents = list(executor.map(fetch_url, raw_urls))
+    url_records = [(url, sitemap_url, crawl_id, True, domain_id) for url in raw_urls]
 
-    parsed_urls = list(map(parse_sitemap, sitemap_contents))
-    flattened_urls = [url for sublist in parsed_urls for url in sublist]
-
-    url_records = []
-    for discovered_url in flattened_urls:
-        logger.debug(f'New URL: {discovered_url} ')
-
-    url_records = [(discovered_url, sitemap_url, crawl_id, True, domain_id) for discovered_url in flattened_urls]
     record_new_urls(url_records)
-    # logger.debug(f'{len(url_records)} URLs added.')
+    logger.debug(f'{len(url_records)} URLs added.')
 
     logger.debug(f'Sitemap {sitemap_id} Complete...')
-    # Update the sitemap status
-   # if update_sitemap_status(sitemap_id, crawl_id):
-   #     logger.debug(f'Sitemap {sitemap_id} Complete...')
-   # else:
-   #     logger.error(f'Sitemap {sitemap_id} not marked complete...')
-
 
 
 if __name__ == '__main__':
-   while True:
-       sitemap_url, sitemap_id, domain_id = next_sitemap_url()
+    while True:
+        sitemap_url, sitemap_id, domain_id = next_sitemap_url()
 
-       if not sitemap_url:
-           break
+        if not sitemap_url:
+            break
 
-       main(sitemap_url, sitemap_id, domain_id)
-
+        main(sitemap_url, sitemap_id, domain_id)
